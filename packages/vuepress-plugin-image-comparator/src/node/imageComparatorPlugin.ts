291100e6135af99d4ea7fb8bc45a749cfe360c63
Alija Sabic <sabic.alija@gmail.com>
2022-05-07 22:35:35 +0200

    vuepress-plugin-image-comparator: update plugin style

--- packages/vuepress-plugin-image-comparator/src/node/imageComparatorPlugin.ts
+++ packages/vuepress-plugin-image-comparator/src/node/imageComparatorPlugin.ts
@@ -13,15 +13,13 @@ export type ImageComparatorPluginOptions = {
   include?: PathSpec[]
 };
 
-export const imageComparatorPlugin = (options : ImageComparatorPluginOptions, app): Plugin => {
-  return {
-    name: "vuepress-plugin-image-comparator",
-    extendsMarkdown: (md) => {
-      md.use(require("./markdown/comparator").default, options)
-    },
-    clientAppEnhanceFiles: [
-      path.resolve(__dirname, '../client/clientAppEnhance.js'),
-      path.resolve(__dirname, '../client/clientAppEnhanceIcons.js')
-    ]
-  }
-}
+export const imageComparatorPlugin = (options : ImageComparatorPluginOptions, app): Plugin => ({
+  name: "vuepress-plugin-image-comparator",
+  extendsMarkdown: (md) => {
+    md.use(require("./markdown/comparator").default, options)
+  },
+  clientAppEnhanceFiles: [
+    path.resolve(__dirname, '../client/clientAppEnhance.js'),
+    path.resolve(__dirname, '../client/clientAppEnhanceIcons.js')
+  ]
+})
