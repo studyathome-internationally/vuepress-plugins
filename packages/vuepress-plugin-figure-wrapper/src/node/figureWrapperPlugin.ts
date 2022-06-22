079da406dd3273c3d960652fc81b92b9d01c78b0
Alija Sabic <sabic.alija@gmail.com>
2022-06-22 19:02:30 +0200

    vuepress-plugin-figure-wrapper: remove missing client configuration file

--- packages/vuepress-plugin-figure-wrapper/src/node/figureWrapperPlugin.ts
+++ packages/vuepress-plugin-figure-wrapper/src/node/figureWrapperPlugin.ts
@@ -9,6 +9,5 @@ export const figureWrapperPlugin = (options: figureWrapperPluginOptions): Plugin
   name: "vuepress-plugin-figure-wrapper",
   extendsMarkdown: (md) => {
     md.use(require("./markdown/wrapper").default, options)
-  },
-  clientConfigFile: path.resolve(__dirname, '../client/config.js')
+  }
 });
